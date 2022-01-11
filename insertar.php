ef7782be0bc6079e9bec27d241dfcdc806659bda
aitageo <aitageo1980@hotmail.com>
2022-01-11 14:11:54 -0500

    el insertar no funciona

--- insertar.php
+++ insertar.php
@@ -5,7 +5,12 @@ if(isset($_POST['nuevo'])){
 $nombre = if(isset($_POST['nombre']));
 $telefono = if(isset($_POST['telefono']));
 $direccion = if(isset($_POST['direccion']));
-$fecha = if(isset($_POST['fecha']));
-echo $nombre;
+$fecha = if(isset($_POST['start']));
+$pdf = if(isset($_FILES['pdf']));
+
+$query = "INSERT INTO usuarios(nombre,telefono,direccion,fecha_atencion,pdf) values('$nombre,$telefono,$direccion,$fecha,$pdf')"
+$result = mysqli_query($conexion,$query);
+header('location:usuarios.html');
+
 }
  ?>
