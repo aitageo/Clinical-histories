b23d9a61e9a9f7f4dfc44b3b7ff03cd038a0265b
aitageo <aitageo1980@hotmail.com>
2022-01-11 19:06:31 -0500

    se uso el atributo formaction para usar los botones hechos con boostrap para llamar archivos php como insertar y eliminar

--- eliminar.php
+++ eliminar.php
@@ -1,5 +1,24 @@
 <?php
-echo "Hola bienvenido";
+include("db.php");
+
+if(isset($_POST['eliminar'])){
+$nombre = $_POST['nombre'];
+
+
+$query = "DELETE FROM usuarios WHERE nombre='$nombre'";
+$result = mysqli_query($conexion,$query);
+
+
+if($result){
+  echo "Borrado exitoso";
+}else {
+  echo "No se pudo borrar";
+}
+
+
+
+
+};
 
 
  ?>
