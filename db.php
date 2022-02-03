80e3ecebcfe4925c218ab8b506addf422a98f4d0
aitageo <aitageo1980@hotmail.com>
2022-02-02 19:35:00 -0500

    cambiando a POO

--- db.php
+++ db.php
@@ -7,7 +7,7 @@
 
 	try {
 
-	$conexion=new PDO('mysql:host=localhost; dbname=historias_clinicas','root','');
+	$conexion=new PDO('mysql:host=localhost; dbname=historias_clinicass','root','');
 
 }catch(Exception $e){
 	die("error" . $e->getMessage());
